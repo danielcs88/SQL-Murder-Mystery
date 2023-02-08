9a7353536231ab86b06a37eca23433ce306a25a6
Daniel Cárdenas <daniel@citypermit.us>
2023-02-08 15:47:01 -0500

    no need for pipe to loc, loc takes lambda

--- .virtual_documents/solution.py
+++ .virtual_documents/solution.py
@@ -1,22 +1,7 @@
-# %% [markdown]
-# # SQL Murder Mystery
-#
-# A crime has taken place and the detective needs your help. The detective gave
-# you the crime scene report, but you somehow lost it.
-#
-# ## Clue 1
-#
-# You vaguely remember that the crime was a **murder** that occurred sometime on
-# **Jan. 15, 2018** and that it took place in **SQL City**. Start by retrieving
-# the corresponding crime scene report from the police department's database. If
-# you want to get the most out of this mystery, try to work through it only
-# using your SQL environment and refrain from using a notepad.
-
-# %%
 import pandas as pd
 from IPython.display import Markdown, display
 
-# %%
+
 drivers_license = pd.read_csv("drivers_license.zip")
 income = pd.read_csv("income.zip")
 get_fit_now_members = pd.read_csv("get_fit_now_members.zip")
@@ -26,7 +11,7 @@ facebook_event_check_in = pd.read_csv("facebook_event_check_in.zip")
 get_fit_now_check_in = pd.read_csv("get_fit_now_check_in.zip")
 crime_scene_report = pd.read_csv("crime_scene_report.zip")
 
-# %%
+
 db = [
     drivers_license,
     income,
@@ -38,10 +23,7 @@ db = [
     crime_scene_report,
 ]
 
-# %% [markdown]
-# ![image.png](https://mystery.knightlab.com/schema.png)
 
-# %%
 deposition = (
     crime_scene_report.assign(
         date=pd.to_datetime(crime_scene_report["date"], format="%Y%m%d")
@@ -52,55 +34,52 @@ deposition = (
 )
 display(deposition)
 
-# %%
+
 display(Markdown("## Clue 2"))
 display(Markdown(deposition["description"].values[0]))
 
 
-# %%
 def namestr(obj, namespace=locals()) -> str:
     return [name for name in namespace if namespace[name] is obj][0]
 
 
-# %%
 def print_table_names():
     print([namestr(d) for d in db])
 
 
-# %%
 for d in db:
     print(namestr(d))
     display(d.sample())
     print()
 
-# %%
+
 witness_1 = person.loc[person["address_street_name"] == "Northwestern Dr"].loc[
     lambda df: df["address_number"] == df["address_number"].max()
 ]
 display(witness_1)
 
-# %%
+
 witness_2 = person.loc[
     (person["name"].str.contains("Annabel"))
     & (person["address_street_name"] == "Franklin Ave")
 ]
 display(witness_2)
 
-# %%
+
 witnesses = pd.concat([witness_1, witness_2])
 
-# %%
+
 clue_3 = interview.loc[interview["person_id"].isin(witnesses["id"])].merge(
     witnesses, left_on="person_id", right_on="id"
 )
 
-# %%
+
 display(Markdown("## Clue 3"))
 for t in clue_3["transcript"]:
     display(Markdown(t))
     print()
 
-# %%
+
 suspect = (
     get_fit_now_members.loc[
         (get_fit_now_members["membership_status"] == "gold")
@@ -125,10 +104,10 @@ suspect = (
     right_on="membership_id",
 )
 
-# %%
+
 display(suspect.T)
 
-# %%
+
 display(Markdown("## Clue 4"))
 display(
     Markdown(
@@ -138,7 +117,7 @@ display(
     )
 )
 
-# %%
+
 mastermind_concert = (
     drivers_license.loc[
         (drivers_license["height"].isin(range(65, 68)))
@@ -162,21 +141,21 @@ mastermind_concert = (
     )
 )
 
-# %%
+
 display(mastermind_concert)
 
-# %%
+
 mastermind = person.loc[
     person["id"].isin(mastermind_concert.drop_duplicates()["person_id"])
 ]
 
-# %%
+
 display(mastermind)
 
-# %%
+
 query = f"INSERT INTO solution VALUES (1, '{mastermind['name'].iloc[0]}');SELECT value FROM solution;"
 print(query)
 
-# %% language="bash"
-#
-# sqlite3 sql-murder-mystery.db "INSERT INTO solution VALUES (1, 'Miranda Priestly');SELECT value FROM solution;"
+
+get_ipython().run_cell_magic("bash", "", """
+sqlite3 sql-murder-mystery.db "INSERT INTO solution VALUES (1, 'Miranda Priestly');SELECT value FROM solution;"""")
